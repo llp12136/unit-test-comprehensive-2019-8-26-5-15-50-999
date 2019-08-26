6c406923c460e995244d91402617e3c0963139ad
llp12136 <liliping@zybank.com.cn>
2019-08-26 20:33:29 +0800

    game

--- src/main/java/tw/Main.java
+++ src/main/java/tw/Main.java
@@ -10,9 +10,12 @@ public class Main {
     private static final int RUN_TIMES = 6;
 
     public static void main(String[] args) throws Exception {
+        GuessInputCommand guessInputCommand = new GuessInputCommand();
         for (int i = 0; i < RUN_TIMES; i++) {
             String input = new GuessInputCommand().input();
-            System.out.println(input);
+           String backString = guessInputCommand.guessNumber(input,"1 2 3 4");
+         System.out.println(backString);
+
         }
     }
 }
--- src/main/java/tw/commands/GuessInputCommand.java
+++ src/main/java/tw/commands/GuessInputCommand.java
@@ -3,6 +3,8 @@ package tw.commands;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * Created by jxzhong on 2017/5/19.
@@ -18,5 +20,24 @@ public class GuessInputCommand {
     public String input() throws IOException {
         System.out.println("------Please input your answer as x x x x , x <10 ------");
         return bufferedReader.readLine();
+    }
+    public String guessNumber(String  input,String answer){
+       // String[] answer = {"1","2","3","4"};
+        int countA=0,countB=0;
+        List<String > answerList = Arrays.asList(answer.split(""));
+        List<String > inputList = Arrays.asList(input.split(" "));
+        for(String number:inputList){
+            //统计相同的数字
+            if(answerList.contains(number)){
+                countB++;
+                //统计位置相同的数字
+                if(answerList.indexOf(number) ==inputList.indexOf(number)){
+                     countA++;
+                     countB--;
+                }
+
+            }
+        }
+        return countA+"A"+countB+"B";
     }
 }
